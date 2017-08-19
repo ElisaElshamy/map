3bfc3ff62881062ea093c242a0e9d511b74f805f
Elisa <elisa9el@gmail.com>
2017-08-19 11:37:37 -0400

    Search filter now working, markers come back when search is cleared

--- js/map.js
+++ js/map.js
@@ -10,13 +10,12 @@ var restaurants = [
 //Foursquare API Keys
 var client_id = '3F2PGSCHKRP5KKKBATD4WPY1XWTPLXXWS4LW1VHXWWTDZXHU';
 var client_secret = 'RVHZN0JBJ0M3Q5Q2CMUJHZHLVYDQWJL2T0L2UADY2IUCZCIC';
-//https://api.foursquare.com/v2/venues/49e4cedbf964a5204f631fe3?ll=40.6,-73.9&client_id=3F2PGSCHKRP5KKKBATD4WPY1XWTPLXXWS4LW1VHXWWTDZXHU&client_secret=RVHZN0JBJ0M3Q5Q2CMUJHZHLVYDQWJL2T0L2UADY2IUCZCIC&v=20170818
 
 var today = new Date();
 //The value returned by getMonth is an integer between 0 and 11
 var current_date = String(today.getFullYear()) + '0' + String(today.getMonth()+1) + String(today.getDate());
 var map;
-//var infowindow;
+
 
 function Marker(location) {
 	this.title = ko.observable(location.name);
@@ -37,7 +36,7 @@ function MapViewModel() {
 	// A neat little trick to have a variable pointer to the VM 
 	var self = this;
 
-	self.markerList = ko.observableArray([]);
+	self.markerList = ko.observableArray();
 
 	restaurants.forEach(function(restaurant){
 		
@@ -98,14 +97,30 @@ function MapViewModel() {
 
 	self.search = ko.observable("");
 
-	self.searchRestaurants = ko.computed(function () {
-        var filter = self.search().toLowerCase();
+	self.restaurantsList = ko.computed(function () {
+        var result = self.search().toLowerCase();
 
-        if (!filter) {
+        //Works as a reset to show all markers if the search box is cleared.
+        //Always checks if markers exist and sets them to visible before
+        //considering the the typed input into the search box.  Idea 
+        //inspired from https://discussions.udacity.com/t/cant-get-to-filter-markers/195121/7
+        self.markerList().forEach(function (item) {
+			if ( item.marker ) {
+				item.marker.setVisible(true);
+			} 
+		});
+
+        if (!result) {
             return self.markerList();
         } else {
+
             return ko.utils.arrayFilter(self.markerList(), function (item) {
-                return item.title().toLowerCase().indexOf(filter) !== -1;
+
+            	if (item.title().toLowerCase().indexOf(result) === -1) {
+            		item.marker.setVisible(false);
+            	}
+
+                return item.title().toLowerCase().indexOf(result) !== -1;
             });
         }
     });
